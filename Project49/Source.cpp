41b74119d81c2e9fea6308d5786281679113e526
lifeIP <97748864+lifeIP@users.noreply.github.com>
2022-03-18 21:03:59 +0700

    +

--- Project49/Source.cpp
+++ Project49/Source.cpp
@@ -20,19 +20,36 @@ void findANDreplace(char str[]) {
 	short i = 0;
 	short ii = strlen(str);
 	char str1[250];
+	short iii;
 	short addres = 0;
 	for (; i < ii - 1;) {
 		strncpy(str1, i + str, 50);
-		short iii = strcspn(str1, " ");
-		
-		i = i+iii+1;
+		iii = strcspn(str1, " ");
+
+		i = i + iii + 1;
 		if (static_cast<int>('А') <= static_cast<int>(str[i]) && static_cast<int>('Я') >= static_cast<int>(str[i])) {
 			if (i > ii) break;
 			addres = i;
 		}
 	}
-	cout << addres << endl;
-	cout << str[addres];
+
+	char str2[250];
+	strncpy(str2, (addres)+str, 50);
+	short iii_p = strcspn(str2, " ");
+	short b = addres + iii_p;
+	short a = addres;
+	cout << str[a];
+	cout << str[addres+iii_p] << endl;
+	char end_resault[250] = {};
+	char addstr[] = { "Кукушечка" };
+	strncpy(end_resault, str, a);
+	strcat(end_resault, addstr);
+	strncpy(str1, b+str, 50);
+	strcat(end_resault, str1);
+
+	cout << end_resault;
+
+
 	
 
 
