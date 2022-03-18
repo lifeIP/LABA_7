30999a047ab77d8fa41adadd08e63962aaa97b49
lifeIP <97748864+lifeIP@users.noreply.github.com>
2022-03-18 22:56:33 +0700

    ++++

--- Project49/Source.cpp
+++ Project49/Source.cpp
@@ -2,27 +2,48 @@
 #include <string.h>
 #include <iostream>
 #include <cstring>
+#include <cmath>
 
 using namespace std;
 
-void findANDreplace(char str[], char resault[]);
+void findANDreplace(char *str, char *resault);
+void convert(float y, char* str);
 
-
-int main() {
+int main()
+{
 	setlocale(LC_ALL, "RUS");
-	char str[] =  "Привет Мир.";
-	char res[250] = {};
-	findANDreplace(str,res);
-	cout << res;
+	float y, z;
+	cout << "Введите y: ";
+	cin >> y;
+	cout << "Введите z: ";
+	cin >> z;
+	char res[50];
+	convert(y, res);
+	cout << res << endl;
+	char* str1 = new char[100];
+	char* str2 = new char[110];
+	strcpy(str1, "Привет мир Почему!");
+	findANDreplace(str1, str2);
+	cout << str1 << endl;
+	cout << str2 << endl;
+	delete[]str1;
+	delete[]str2;
+}
+void convert(float y, char* str)
+{
+	double x = sin(3 * y)+16.5;
+	sprintf(str, "%08.8f = sin(3* %08.8f)+16.5", x, y);
 }
 
-
-
-
-
-
-
-void findANDreplace(char str[], char resault[]) {
+void findANDreplace(char* str_l, char* resault_l) {
+	for (int i = 0; i < strlen(resault_l); i++) {
+		resault_l[i] = 0;
+	}
+	
+	char str[250] = {};
+	for (short i = 0; i < strlen(str_l); i++) {
+		str[i] = str_l[i];
+	}
 	short i = 0;
 	short ii = strlen(str);
 	char str1[250];
@@ -39,7 +60,7 @@ void findANDreplace(char str[], char resault[]) {
 		}
 	}
 
-	char str2[250];
+	char str2[250] = {};
 	strncpy(str2, (addres)+str, 50);
 	short iii_p = strcspn(str2, " ");
 	short b = addres + iii_p;
@@ -51,5 +72,10 @@ void findANDreplace(char str[], char resault[]) {
 	strcat(end_resault, addstr);
 	strncpy(str1, b+str, 50);
 	strcat(end_resault, str1);
-	strcat(resault, end_resault);
+	for (int i = 0; i < strlen(end_resault); i++)
+	{
+		resault_l[i] = str[i];
+	}
+	
+
 }
